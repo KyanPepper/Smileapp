4ffd343ee40c56830d010fcb97ee6a72af7a889f
KyanPepper <kyankks1@gmail.com>
2023-09-29 22:20:21 -0700

    Added moment and times last task

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager
+from flask_moment import Moment
 # TODO: (milestone 3) import LoginManager and Moment extensions here
 login = LoginManager()
 login.login_view = 'auth.login'
 db = SQLAlchemy()
+moment = Moment()
 # TODO: (milestone 3) create LoginManager object and configure the login view as 'auth.login', i.e, `login` route in `auth` Blueprint. 
 # TODO: (milestone 3) create Moment object
 
@@ -14,7 +16,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     app.static_folder = config_class.STATIC_FOLDER 
     app.template_folder = config_class.TEMPLATE_FOLDER
-
+    moment.init_app(app)
     db.init_app(app)
     login.init_app(app)
     # TODO: (milestone 3) Configure the app object for login using `init_app` function. 
